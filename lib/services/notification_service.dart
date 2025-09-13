883dfb6ce1a65c08827a1effeeb4ab08211e5c26
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-13 21:57:18 +0530

    Update scheduleMaintenanceNotification to use exactAllowWhileIdle mode for Android

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -124,7 +124,7 @@ class NotificationService {
       'Your $treeSpecies tree needs a $maintenanceType. Earn more EcoCoins!',
       tz.TZDateTime.from(scheduledDate, tz.local),
       notificationDetails,
-      androidAllowWhileIdle: true,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime,
       payload: 'maintenance_$id',
