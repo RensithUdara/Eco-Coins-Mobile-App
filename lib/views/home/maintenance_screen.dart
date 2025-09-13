8f0ebe27059fff67222263d0728d97da109a0a0a
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-14 00:32:43 +0530

    Add null safety handling for selected image in maintenance screen

--- lib/views/home/maintenance_screen.dart
+++ lib/views/home/maintenance_screen.dart
@@ -1553,6 +1553,9 @@ class _MaintenanceScreenState extends State<MaintenanceScreen> {
     if (_selectedImage == null) {
       return _buildImagePlaceholder();
     }
+    
+    // Safely capture the image file to avoid null issues later
+    final File imageFile = _selectedImage!;
 
     return Stack(
       children: [
